256e888981a295b27a3c85339c141a8ace700d1e
ValeryDrozd <61665987+ValeryDrozd@users.noreply.github.com>
2020-12-09 16:38:12 +0200

    Study version of webpack

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+module.exports = {
+    entry: {main:'./src/index.js',slider:'./src/js/slider.js'},
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname,'dist')
+    }
+}
